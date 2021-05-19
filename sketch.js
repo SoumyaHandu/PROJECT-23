61456c969ffc261c560889884e07a502bf1e54ea
SoumyaHandu <75081856+SoumyaHandu@users.noreply.github.com>
2021-05-19 15:25:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var helicopterImg, helicopter;
+var package, packageImg;
+var packageBody, ground;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterImg = loadImage("helicopter.png");
+	packageImg = loadImage("package.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+
+	package = createSprite(width/2, 80, 10,10);
+	package.addImage(packageImg);
+	package.scale = 0.2;
+
+	helicopter = createSprite(width/2, 200, 10,10);
+	helicopter.addImage(helicopterImg);
+	helicopter.scale = 0.6;
+
+	ground = createSprite(width/2, height-35, width,10);
+	ground.shapeColor = color(255)
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2, 200, 5, {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+	ground = Bodies.rectangle(width/2, 650, width, 10, {isStatic:true} );
+ 	World.add(world, ground);
+
+	Engine.run(engine);
+  
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  package.x= packageBody.position.x 
+  package.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+
+  Matter.Body.setStatic(packageBody,false);  
+  }
+}
